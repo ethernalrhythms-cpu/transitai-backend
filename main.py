536af829a15b0dad5aef0f4240d5843cdf0cadde
DHEEPTHI KI <collosal947@gmail.com>
2026-05-11 00:45:39 +0530

    fix cors

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://transitai-frontend.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
